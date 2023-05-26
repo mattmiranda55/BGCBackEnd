89bbef5d2f97c9be345f73346a712bf0bc235b22
mattmiranda55 <92895725+mattmiranda55@users.noreply.github.com>
2023-05-26 09:10:04 -0400

    Update settings.py

--- BGC_Back_End/settings.py
+++ BGC_Back_End/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": "Bowler55!",
+        "PASSWORD": "",
         "HOST": "localhost",
         "PORT": "5432",
     }
