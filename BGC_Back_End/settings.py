e5602d1d8c333e44a36c2823acf13ffd42b6a7fe
mattmiranda55 <mattmiranda55@gmail.com>
2023-06-13 13:18:26 -0400

    new db connection

--- BGC_Back_End/settings.py
+++ BGC_Back_End/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": "",
+        "PASSWORD": "BGCadmin123db",
         "HOST": "localhost",
         "PORT": "5432",
     }
