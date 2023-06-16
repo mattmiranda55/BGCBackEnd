81e37d2c817bf0e676b8893104f4735dba640b75
mattmiranda55 <mattmiranda55@gmail.com>
2023-06-16 11:34:23 -0400

    add ec2 instance connection

--- BGC_Back_End/settings.py
+++ BGC_Back_End/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+vvehwdph0u_i=zz=9$8gmb6_rub@t)zq7#37)(_a^nvh249z+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['44.204.149.217']
 
 
 # Application definition
